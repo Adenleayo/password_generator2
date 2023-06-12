dcae00e48eec3e3e49035df62c32ff1d9e87a8f2
Adenleayo <115345947+Adenleayo@users.noreply.github.com>
2023-06-12 13:21:01 +0100

    password generator

---
+++ password_generator2.py
@@ -0,0 +1,25 @@
+import string
+import random 
+
+def generate_password():
+    password= input('pls input your password: ')
+    length = 0 
+    if password == 'weak':
+        length += 5
+    elif password == 'strong':
+        length += 8 
+    elif password == 'very strong':
+        length += 15
+    else:
+        print('pls input weak,strong,verystrong to generate random password')    
+        
+    save = string.ascii_uppercase + string.ascii_lowercase + string.digits + string.punctuation
+    passwor = ''.join(random.choice(save)for i in range(length))
+    return passwor
+
+
+new_password = generate_password()
+
+print(f'this is your password  : {new_password}')       
+        
+               
